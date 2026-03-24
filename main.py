53a1cff15b305321803f0f01d1e95e2740914134
Frédéric Vlummens <frederic.vlummens@howest.be>
2026-03-24 13:33:48 +0100

    Add port binding

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,5 @@ def greet(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
